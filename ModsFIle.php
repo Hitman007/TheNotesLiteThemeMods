9ebe57b98a8c326e24b108ae637af163e99cbbca
Hitman007 <support@customrayguns.com>
2016-10-07 01:34:18 -0700

    vvvv

--- ModsFIle.php
+++ ModsFIle.php
@@ -21,7 +21,7 @@ $output = <<<OUTPUT
 	jQuery("document").ready(function(){
 		alert('jQuery');
 		//jQuery(".footer-copyright-wrapper").hide();
-		jQuery(".footer-copyright-wrapper").html("<span style="float:left;">Copyright <a href = "http://jimsbigbrain.com">&copy;</a> Jim\'s Big Brain. All rights reserved. <a href = "http://jimsbigbrain.com/contact/">Contact us.</a> This site is a <a href = "http://customrayguns.com">Custom Ray Gun.<img align = "MIDDLE" src = "http://nevadacathouse.biz/wp-content/uploads/2012/12/ray_gun.gif" /></a>.</span>");
+		//jQuery(".footer-copyright-wrapper").html("<span style="float:left;">Copyright <a href = "http://jimsbigbrain.com">&copy;</a> Jim\'s Big Brain. All rights reserved. <a href = "http://jimsbigbrain.com/contact/">Contact us.</a> This site is a <a href = "http://customrayguns.com">Custom Ray Gun.<img align = "MIDDLE" src = "http://nevadacathouse.biz/wp-content/uploads/2012/12/ray_gun.gif" /></a>.</span>");
 	});
 </script>
 OUTPUT;
