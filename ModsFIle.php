c6ad07999e28ced558327898535c81cdfab20ea1
Hitman007 <support@customrayguns.com>
2016-10-07 01:47:09 -0700

    ddd

--- ModsFIle.php
+++ ModsFIle.php
@@ -8,21 +8,18 @@ Author: Jim Maguire
 Author URI: http://customrayguns.com/
 */
 
-//namespace TheNotesLiteThemeMods;
+namespace TheNotesLiteThemeMods;
 
 
 
-add_action( 'wp_footer', 'your_function' );
+add_action( 'wp_footer', '\TheNotesLiteThemeMods\recomveCopyrightInfo' );
 	
-function your_function(){
+function recomveCopyrightInfo(){
 
 $output = <<<OUTPUT
 <script>
-	//alert('HI');
 	jQuery("document").ready(function(){
-		alert('jQuery');
-		//jQuery(".footer-copyright-wrapper").hide();
-		jQuery(".footer-copyright-wrapper").html('<span style="float:left;">Copyright <a href = "http://jimsbigbrain.com">&copy;</a> Jim\'s Big Brain. All rights reserved. <a href = "http://jimsbigbrain.com/contact/">Contact us.</a> This site is a <a href = "http://customrayguns.com">Custom Ray Gun.<img align = "MIDDLE" src = "http://nevadacathouse.biz/wp-content/uploads/2012/12/ray_gun.gif" /></a>.</span>');
+		jQuery(".footer-copyright-wrapper").html('<span style="float:left;">Copyright <a href = "http://jimsbigbrain.com">&copy;</a> Jim\'s Big Brain. All rights reserved. <a href = "http://customrayguns.com/contact/">Contact us.</a> This site is a <a href = "http://customrayguns.com">Custom Ray Gun.<img align = "MIDDLE" src = "http://customrayguns.com/wp-content/uploads/2012/08/ray_gun.gif" /></a>.</span>');
 	});
 </script>
 
