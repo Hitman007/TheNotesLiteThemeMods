0831783b7ff0fe2062717d624013e83f9cc4aa83
Hitman007 <support@customrayguns.com>
2016-10-07 00:56:39 -0700

    mods

---
+++ ModsFIle.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: TheNotesLiteThemeMods
+Plugin URI: http://customrayguns.com/
+Description: 
+Version: 0.1
+Author: Jim Maguire
+Author URI: http://customrayguns.com/
+*/
+
+namespace TheNotesLiteThemeMods;
+
+
+	footer-copyright-wrapper
