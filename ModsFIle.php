9b4378a3e153c0221f7b2f97d776bbb80328a2a9
Hitman007 <support@customrayguns.com>
2016-10-07 01:09:21 -0700

    fixed

--- ModsFIle.php
+++ ModsFIle.php
@@ -8,14 +8,16 @@ Author: Jim Maguire
 Author URI: http://customrayguns.com/
 */
 
-namespace TheNotesLiteThemeMods;
+//namespace TheNotesLiteThemeMods;
+
+
 
 add_action( 'wp_footer', 'your_function' );
 
 	//footer-copyright-wrapper
 	
 function your_function(){
-	die('line 18');
+	//die('line 18');
 echo '
 <script>
 	//jQuery("document").ready(function(){
