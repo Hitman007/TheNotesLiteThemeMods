4f2f745e0be09dde26835558e803da599b2a63ac
Hitman007 <support@customrayguns.com>
2016-10-09 13:27:38 -0700

    Update ModsFIle.php

--- ModsFIle.php
+++ ModsFIle.php
@@ -22,6 +22,9 @@ $output = <<<OUTPUT
 		jQuery(".footer-copyright-wrapper").html('<span style="float:left;">Copyright <a href = "http://jimsbigbrain.com">&copy;</a> Jim\'s Big Brain. All rights reserved. <a href = "http://customrayguns.com/contact/">Contact us.</a> This site is a <a href = "http://customrayguns.com">Custom Ray Gun.<img align = "MIDDLE" src = "http://customrayguns.com/wp-content/uploads/2012/08/ray_gun.gif" /></a>.</span>');
 	});
 </script>
+<style>
+     a {text-decoration:underline !important;}
+</style>
 
 OUTPUT;
 
