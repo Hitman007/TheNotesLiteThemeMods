3f702dda75027c0b7cacfedd96a017ba2fc076e8
Hitman007 <support@customrayguns.com>
2017-03-20 15:24:54 -0700

    Update ModsFIle.php

--- ModsFIle.php
+++ ModsFIle.php
@@ -16,10 +16,13 @@ add_action( 'wp_footer', '\TheNotesLiteThemeMods\recomveCopyrightInfo' );
 	
 function recomveCopyrightInfo(){
 
+$rayGunIMG_URL = site_url() . "/wp-content/plugins/TheNotesLiteThemeMods/img/ray_gun.png";
+
 $output = <<<OUTPUT
 <script>
 	jQuery("document").ready(function(){
-		jQuery(".footer-copyright-wrapper").html('<span style="float:left;">Copyright <a href = "http://jimsbigbrain.com">&copy;</a> Jim\'s Big Brain. All rights reserved. <a href = "http://customrayguns.com/contact/">Contact us.</a> This site is a <a href = "http://customrayguns.com">Custom Ray Gun.<img align = "MIDDLE" src = "http://customrayguns.com/wp-content/uploads/2012/08/ray_gun.gif" /></a>.</span>');
+		jQuery(".footer-copyright-wrapper").html('<span style="float:left;">Copyright <a href = "http://messagetothefish.com">&copy;</a> Message To The Fish. All rights reserved. This site is a <a href = "http://customrayguns.com">Custom Ray Gun.<img align = "MIDDLE" src = "
+$rayGunIMG_URL" /></a>.</span>');
 	});
 </script>
 <style>
