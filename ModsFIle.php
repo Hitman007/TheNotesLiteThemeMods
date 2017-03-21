b512bd431a044676b3a5ad3a6209cfcd569c03f0
Hitman007 <support@customrayguns.com>
2017-03-20 17:47:13 -0700

    Update ModsFIle.php

--- ModsFIle.php
+++ ModsFIle.php
@@ -10,19 +10,18 @@ Author URI: http://customrayguns.com/
 
 namespace TheNotesLiteThemeMods;
 
-die ('hello');
+//die ('hello');
 
 add_action( 'wp_footer', '\TheNotesLiteThemeMods\recomveCopyrightInfo' );
 	
 function recomveCopyrightInfo(){
 
-$rayGunIMG_URL = site_url() . "/wp-content/plugins/TheNotesLiteThemeMods/img/ray_gun.png";
+$rayGunIMG_URL = "http://messagetothefish.com/wp-content/plugins/TheNotesLiteThemeMods/img/ray_gun.png";
 
 $output = <<<OUTPUT
 <script>
 	jQuery("document").ready(function(){
-		jQuery(".footer-copyright-wrapper").html('<span style="float:left;">Copyright <a href = "http://messagetothefish.com">&copy;</a> Message To The Fish. All rights reserved. This site is a <a href = "http://customrayguns.com">Custom Ray Gun.<img align = "MIDDLE" src = "
-$rayGunIMG_URL" /></a>.</span>');
+		jQuery(".footer-copyright-wrapper").html('<span style="float:left;">Copyright <a href = "http://messagetothefish.com">&copy;</a> Message To The Fish. All rights reserved. This site is a <a href = "http://customrayguns.com">Custom Ray Gun.<img align = "MIDDLE" src = "$rayGunIMG_URL" /></a>.</span>');
 	});
 </script>
 <style>
